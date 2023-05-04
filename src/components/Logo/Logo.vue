567d96b055d9c50cc595a0542b5b19e798c17917
ikl0 <klimovs2008van@gmail.com>
2023-05-04 16:57:01 +0300

    change color

--- src/components/Logo/Logo.vue
+++ src/components/Logo/Logo.vue
@@ -8,7 +8,6 @@
 h3
   font-size: 24px
   text-align: center
-  color: rgb(238, 174, 202)
-  color: radial-gradient(circle,rgb(221, 174, 238) 0%,rgba(148, 187, 233, 1) 100%)
+  color: black
   padding-top: 20px
 </style>
